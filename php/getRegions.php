d4b69c1a2305a0fa813934c05003332eb2c8b818
davbran <70201910+davbran@users.noreply.github.com>
2021-03-11 14:07:28 +0000

    Update getRegions.php

--- php/getRegions.php
+++ php/getRegions.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://api.geonames.org/searchJSON?country=".$_REQUEST['countryCode']."&countryBias=".$_REQUEST['countryCode']."&featureClass=A&maxRows=40&orderby=relevance&username=davbranciamore";
+$url = "http://api.geonames.org/searchJSON?country=".$_REQUEST['countryCode']."&countryBias=".$_REQUEST['countryCode']."&featureClass=A&maxRows=40&orderby=relevance&username={apiKey}";
 
 $ch = curl_init();
 
